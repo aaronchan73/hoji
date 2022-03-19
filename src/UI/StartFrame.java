a74e17c4518aea79972576bff52e13008f1aa864
Gloria <gfung01@students.cs.ubc.ca>
2022-03-18 20:33:02 -0700

    added image to startFrame

--- src/UI/StartFrame.java
+++ src/UI/StartFrame.java
@@ -16,46 +16,78 @@ public class StartFrame implements ActionListener {
     public static int HEIGHT = 500;
 
     public JFrame startFrame;
-    public ViewDeck viewDeck; 
+    // public ViewDeck viewDeck; 
     private JButton start; 
-    private BufferedImage startBackground; 
+    // private ImageIcon imgIcon;
+    // private Image img;  
     private JLabel startBackgroundLabel; 
+    private BufferedImage startBackground; 
+    private JPanel startPanel; 
+    private SplashScreen splash; 
     
+    // public class BackgroundImage extends JPanel { 
+    //     Image Pic; 
+    
+    //     public BackgroundImage() { 
+    //         ImageIcon obj = new ImageIcon("./stardBackground.png"); 
+    //         Pic = obj.getImage(); 
+    //     }
+    
+    //     public void paintComponent(Graphics g) { 
+    //         super.paintComponent(g); 
+    //         g.drawImage(Pic, 0, 0, null); 
+    //     }
+    // }
 
     public StartFrame() {
         initStartFrame(); 
     } 
 
     public void initStartFrame() {
-        startFrame = new JFrame();
-        startFrame.setVisible(true);
+        startFrame = new JFrame(); 
+        // BackgroundImage Pic = new BackgroundImage(); 
+        startPanel = new JPanel(new GridBagLayout());
+        startFrame.add(startPanel); 
+        
+        // imgIcon = new ImageIcon("startBackground.png"); 
+    
         startFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         startFrame.setSize(WIDTH, HEIGHT); 
+        // startPanel.add(Pic); 
         initStartButtons(); 
+        startFrame.setVisible(true); 
 
-        try { // background image 
+       // background image  
+        try { 
             startBackground = ImageIO.read(new File("./startBackground.png"));
         } catch (IOException e) {
-            e.printStackTrace();
+            e.printStackTrace(); 
         }
         startBackgroundLabel = new JLabel(new ImageIcon(startBackground));
-        startBackgroundLabel.setBounds(0, 0, 500, 500); 
+        startBackgroundLabel.setLayout(new GridBagLayout());
+        startBackgroundLabel.setBounds(0, 0, 500, 500);  
+
+        startPanel.add(startBackgroundLabel); 
     }
     
     public void initStartButtons() { 
         start = new JButton("Begin customizing your deck");
-        start.setSize(new Dimension(300, 300));
+        start.setSize(300, 300);
         start.addActionListener(this);
-        startFrame.add(start); 
+        startPanel.add(start); 
     } 
 
     public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == start) {
-            startFrame.setVisible(false); 
-            viewDeck = new ViewDeck(); 
-            viewDeck.viewDeck.setVisible(true); 
-	    }
+		// if (e.getSource() == start) {
+        //     startFrame.setVisible(false); 
+        //     viewDeck = new ViewDeck(); 
+        //     viewDeck.viewDeck.setVisible(true); 
+	    // }
     }
+    
+    // public void paintComponent(Graphics g) { 
+    //     super.paintComponent(g); 
+    //     g.drawImage(img, 0, 0, null); 
+    // }
+} 
 
-
-}
